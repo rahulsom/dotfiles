4ca8df4bd88e25f58ffd375e2c166d8ae7ba839b
Rahul Somasunderam <rahul.som@gmail.com>
2014-10-22 19:15:52 -0700

    Better jvm switcher

--- bin/jvm.sh
+++ bin/jvm.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
+toJava() {
+  JDKDIR=$(basename $(dirname $(dirname $1)))
+  JDKVER=$(echo $JDKDIR| sed -e "s/[A-Za-z]//g" | sed -e "s/^1.//g" | sed -e "s/\.$//g")
+  if [ $(echo $JDKVER | grep -c _) = 0 ]; then
+    JDKVER=${JDKVER}_0
+  fi
+  JDKVER=$(echo $JDKVER | sed -e "s/\.0_/u/g")
+  printf "%10s   %s\n" "$JDKVER" "$1"
+}
 
 listJavaHomes() {
-  echo "Choose from:" >&2
   find /Library/Java -name "Home" -type d | grep -i java
   find /System/Library/Java/JavaVirtualMachines -name "Home" -type d | grep -i java
 }
 
-echo "Before: JAVA_HOME=${JAVA_HOME}" >&2
+findJava() {
+  listJavaHomes | while read -r i; do toJava "$i"; done | grep $1 | tr -s " " | cut -d " " -f 3
+}
+
+
 if [ "$1" = "" ]; then
-  listJavaHomes
+  echo "Choose from:" >&2
+  echo ""
+  listJavaHomes | while read -r i; do toJava "$i"; done | sort -k 1 -n
+  echo ""
+  echo "${BACKGROUND_BLUE}${TEXT_WHITE}!JAVA_HOME=${JAVA_HOME}${RESET_FORMATTING}" >&2
 elif [ "$1" = "-u" ]; then
+  echo "${BACKGROUND_RED}${TEXT_WHITE}-JAVA_HOME=${JAVA_HOME}${RESET_FORMATTING}" >&2
   unset JAVA_HOME
+  echo "${BACKGROUND_GREEN}${TEXT_WHITE}+JAVA_HOME=${JAVA_HOME}${RESET_FORMATTING}" >&2
 else
-  export JAVA_HOME=$1
+  echo "${BACKGROUND_RED}${TEXT_WHITE}-JAVA_HOME=${JAVA_HOME}${RESET_FORMATTING}" >&2
+  export JAVA_HOME=$(findJava $1)
+  echo "${BACKGROUND_GREEN}${TEXT_WHITE}+JAVA_HOME=${JAVA_HOME}${RESET_FORMATTING}" >&2
 fi
-echo "After : JAVA_HOME=${JAVA_HOME}" >&2
