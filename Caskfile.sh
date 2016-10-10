1fee5478f7e1f897baf89e91703be929585c47a6
Rahul Somasunderam <rahul.som@gmail.com>
2016-10-10 10:04:48 -0700

    Replace dockertoolbox with docker
    That will install docker for mac

--- Caskfile.sh
+++ Caskfile.sh
@@ -12,7 +12,7 @@ brew cask install vagrant
 brew cask install java
 brew cask install java6
 brew cask install java7
-brew cask install dockertoolbox
+brew cask install docker
 brew cask install firefox
 brew cask install firefoxdeveloperedition
 brew cask install intellij-idea
