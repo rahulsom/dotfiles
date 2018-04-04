a226a549246e76d6ec618ff770e4ed0e6c263544
Rahul Somasunderam <rsomasunderam@netflix.com>
2018-04-04 12:46:08 -0700

    Stop installing java, firefox
    Also install iterm2

--- Caskfile.sh
+++ Caskfile.sh
@@ -10,17 +10,12 @@ brew cask install google-chrome
 brew cask install atom
 brew cask install virtualbox
 brew cask install vagrant
-brew cask install java
-brew cask install java6
-brew cask install java7
 brew cask install docker
-brew cask install firefox
-brew cask install firefoxdeveloperedition
 brew cask install intellij-idea
-brew cask install hipchat
 brew cask install slack
 brew cask install github-desktop
 brew cask install lighttable
+brew cask install iterm2
 brew cask install xquartz
 
 brew cask install font-fira-code
