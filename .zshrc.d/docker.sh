667f576a4ec649257582eebf3286f36410cc6446
Rahul Somasunderam <rsomasunderam@netflix.com>
2018-05-10 22:32:42 -0700

    Add tesseract 4

--- .zshrc.d/docker.sh
+++ .zshrc.d/docker.sh
@@ -130,3 +130,5 @@ alias ubuntu='docker run --rm -it --hostname ubuntu -v $(pwd):/local -w /local u
 alias centos='docker run --rm -it --hostname centos -v $(pwd):/local -w /local centos bash'
 alias openjdk='docker run --rm -it --hostname openjdk -v $(pwd):/local -w /local openjdk:8-alpine sh'
 alias alpine='docker run --rm -it --hostname alpine -v $(pwd):/local -w /local alpine sh'
+
+alias t4re='docker run --rm -it -v $(pwd):/home/work tesseractshadow/tesseract4re tesseract'
